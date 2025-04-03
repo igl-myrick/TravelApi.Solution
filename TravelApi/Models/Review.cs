f8d0928481b5bf727ec779a2bffa2ed6edfe1b59
igl-myrick <igl.myrick@gmail.com>
2025-04-03 15:56:32 -0700

    add Rating property and validation

--- TravelApi/Models/Review.cs
+++ TravelApi/Models/Review.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TravelApi.Models
 {
   public class Review
   {
     public int ReviewId { get; set; }
     public string Body { get; set; }
+    [Required]
+    [Range(1, 5, ErrorMessage = "Rating must be a whole number between 1 and 5.")]
+    public int Rating { get; set; }
     public string Country { get; set; }
     public string City { get; set; }
     public string UserName { get; set; }

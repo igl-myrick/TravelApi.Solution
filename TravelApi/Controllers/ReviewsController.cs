3303bf83902ce038f5a62e68c6aa25b2fbb47686
igl-myrick <igl.myrick@gmail.com>
2025-04-04 15:10:38 -0700

    add Get route

---
+++ TravelApi/Controllers/ReviewsController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelApi.Models;
+
+namespace TravelApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ReviewsController : ControllerBase
+  {
+    private readonly TravelApiContext _db;
+
+    public ReviewsController(TravelApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Review>>> Get(string country, string city)
+    {
+      IQueryable<Review> query = _db.Reviews.AsQueryable();
+
+      if (country != null)
+      {
+        query = query.Where(entry => entry.Country == country);
+      }
+
+      if (city != null)
+      {
+        query = query.Where(entry => entry.City == city);
+      }
+
+      return await query.ToListAsync();
+    }
+  }
+}

5277add39c86c80172a59c00079e2d3c4440993a
igl-myrick <igl.myrick@gmail.com>
2025-04-04 13:37:17 -0700

    add model validation

--- TravelApi/Models/Review.cs
+++ TravelApi/Models/Review.cs
@@ -5,12 +5,16 @@ namespace TravelApi.Models
   public class Review
   {
     public int ReviewId { get; set; }
+    [Required]
     public string Body { get; set; }
     [Required]
     [Range(1, 5, ErrorMessage = "Rating must be a whole number between 1 and 5.")]
     public int Rating { get; set; }
+    [Required]
     public string Country { get; set; }
+    [Required]
     public string City { get; set; }
+    [Required]
     public string UserName { get; set; }
   }
 }

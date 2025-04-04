5314618b934af8bbc3474a40c568b2c7603810b5
igl-myrick <igl.myrick@gmail.com>
2025-04-04 16:00:35 -0700

    add DeleteReview route

--- TravelApi/Controllers/ReviewsController.cs
+++ TravelApi/Controllers/ReviewsController.cs
@@ -87,5 +87,20 @@ namespace TravelApi.Controllers
     {
       return _db.Reviews.Any(e => e.ReviewId == id);
     }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReview(int id)
+    {
+      Review review = await _db.Reviews.FindAsync(id);
+      if (review == null)
+      {
+        return NotFound();
+      }
+
+      _db.Reviews.Remove(review);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
   }
 }

0a1bbedfa5869aadda2e622a95205fce19a3fb43
igl-myrick <igl.myrick@gmail.com>
2025-05-20 15:43:50 -0700

    move model properties to new RegisterModel class

--- TravelApi/Models/ApplicationUser.cs
+++ TravelApi/Models/ApplicationUser.cs
@@ -1,13 +1,8 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace TravelApi.Models
 {
   public class ApplicationUser : IdentityUser
   {
-    [Required]
-    [DataType(DataType.Password)]
-    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$", ErrorMessage = "Your password must contain at least six characters, a capital letter, a lowercase letter, a number, and a special character.")]
-    public string Password { get; set; }
   }
 }

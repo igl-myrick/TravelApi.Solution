456b73921d69fdae9f919cd07fda71f90b196065
igl-myrick <igl.myrick@gmail.com>
2025-05-20 15:44:31 -0700

    add RegisterModel class

---
+++ TravelApi/Migrations/20250520214330_AddRegisterModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelApi.Migrations
+{
+    public partial class AddRegisterModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}

57c6b61754046e77a15519231b820966e881cac9
igl-myrick <igl.myrick@gmail.com>
2025-05-21 15:58:50 -0700

    add wip Login route

--- TravelApi/Controllers/AuthController.cs
+++ TravelApi/Controllers/AuthController.cs
@@ -40,5 +40,48 @@ namespace TravelApi.Controllers
         return BadRequest();
       }
     }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, isPersistent: false, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        string issuer = _configuration["Jwt:Issuer"];
+        string audience = _configuration["Jwt:Issuer"];
+        byte[] key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+        SigningCredentials signingCredentials = new SigningCredentials(
+          new SymmetricSecurityKey(key),
+          SecurityAlgorithms.HmacSha512Signature
+        );
+
+        ClaimsIdentity subject = new ClaimsIdentity(new[]
+        {
+          new Claim(JwtRegisteredClaimNames.Sub, model.Username),
+          new Claim(JwtRegisteredClaimNames.Email, model.Username),
+        });
+
+        DateTime expires = DateTime.UtcNow.AddMinutes(10);
+
+        SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+        {
+          Subject = subject,
+          Expires = expires,
+          Issuer = issuer,
+          Audience = audience,
+          SigningCredentials = signingCredentials
+        };
+
+        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = tokenHandler.WriteToken(token);
+
+        return Ok(jwtToken);
+      }
+      else
+      {
+        return BadRequest("There is something wrong with your username or password.");
+      }
+    }
   }
 }
